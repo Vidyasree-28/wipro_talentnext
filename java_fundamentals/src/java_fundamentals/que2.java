883bbd07b251c4b5d3919fb4c01c739d21c64daf
Vidyasree-28 <vidyasree2802@gmail.com>
2025-07-04 19:57:14 +0530

    java_fundamentals

---
+++ java_fundamentals/src/java_fundamentals/que2.java
@@ -0,0 +1,10 @@
+package java_fundamentals;
+import java.util.Scanner;
+public class que2 {
+	public static void main(String[] args) {
+		Scanner th=new Scanner(System.in);
+		String str1=th.nextLine();
+		System.out.println("Welcome " + str1);
+	}
+
+}
---
+++ java_fundamentals/src/java_fundamentals/question6.java
@@ -0,0 +1,9 @@
+package java_fundamentals;
+import java.util.*;
+public class question6 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String str=(" ");
+	}
+
+}
